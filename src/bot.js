f4ce03029d405af41d4d17207e06f1d2a7b1a02e
Cha'sMacAir <chayh311@gmail.com>
2020-08-29 01:42:47 +0900

    feat: Add create commend and set rules
    (checking validation)

--- src/bot.js
+++ src/bot.js
@@ -6,6 +6,8 @@ const client = new Discord.Client();
 
 const PREFIX = "$";
 
+let isCreated = false;
+
 client.on('ready', () => {
     console.log(`${client.user.tag} has logged in.`);
 })
@@ -19,6 +21,25 @@ client.on('message', msg => {
 
         console.log('CMD_NAME', CMD_NAME);
         console.log('args', args);
+        
+        if(CMD_NAME === 'create') {
+            //NOTE : check validation
+            if(args[0].match(/[^0-9]/g)) {
+                msg.channel.send('잘못된 입력입니다. "$create 3" 과 같이 명령어 뒤에 숫자를 입력해주세요.');
+                return;
+            }
+            if(args[0]<4) {
+                msg.channel.send('너무 작은 수를 입력하였습니다. 게임 시작의 최소인원은 4명입니다. 명령어 뒤에 숫자를 4이상으로 입력해주세요.');
+                return;
+            }
+            if(args[0]>10) {
+                msg.channel.send('너무 많은 수를 입력하였습니다. 게임 시작의 최대인원은 10명입니다. 명령어 뒤에 숫자를 10이하로 입력해주세요.');
+                return;
+            }
+        
+            //NOTE : let createFlag be true
+            isCreated = true;
+        }
     }
   });
 
