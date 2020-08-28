a59fe6671457ed3f08ae01fbeee76876f05c16c7
Cha'sMacAir <chayh311@gmail.com>
2020-08-29 04:03:51 +0900

    feat: Add start command
    add creating channel function
    update making roles funtion
    add deleting created things funtion

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
@@ -7,7 +6,10 @@ const client = new Discord.Client();
 const PREFIX = "$";
 
 let isCreated = false;
-let joinedUsers = [];
+let joinedUserIds = [];
+let createdChannelIds = [];
+let createdRoleIds = [];
+let mafiaId;
 
 client.on('ready', () => {
     console.log(`${client.user.tag} has logged in.`);
@@ -37,30 +39,18 @@ client.on('message', msg => {
                 msg.channel.send('너무 많은 수를 입력하였습니다. 게임 시작의 최대인원은 10명입니다. 명령어 뒤에 숫자를 10이하로 입력해주세요.');
                 return;
             }
-        
+            
             //NOTE : let createFlag be true
             isCreated = true;
-
-            //NOTE : make roles
-            for(let i = 0; i < args[0]-1; i++) {
-                msg.guild.roles.create({
-                    data: {
-                        name: `role[${i}]`,
-                        position: 1,
-                        color: 'GOLD',
-                        permissions: ['MANAGE_ROLES']
-                    }
-                }).then(d => {console.log('d', d)});
-            }
         }
 
         if(CMD_NAME === 'join') {
             const authorId = msg.author.id;
-            const index = joinedUsers.findIndex(userId => userId === authorId);
+            const index = joinedUserIds.findIndex(userId => userId === authorId);
             console.log('index', index);
             const member = msg.guild.members.cache.get(authorId);
             if(index === -1) {
-                joinedUsers.push(authorId);
+                joinedUserIds.push(authorId);
                 msg.channel.send(`${member.user.username}님이 참여하였습니다.`);
             } else {
                 msg.channel.send(`${member.user.username}님은 이미 참여한 상태입니다.`);
@@ -68,13 +58,13 @@ client.on('message', msg => {
         }
 
         if(CMD_NAME === 'status') {
-            const joinedUserCount = joinedUsers.length;
+            const joinedUserCount = joinedUserIds.length;
         
             msg.channel.send(`Joined user count: ${joinedUserCount}`);
             let joinedUserNames = [];
-            joinedUsers.length
+            joinedUserIds.length
                 ? 
-                joinedUsers.forEach(userId => {
+                joinedUserIds.forEach(userId => {
                     const member = msg.guild.members.cache.get(userId);
                     joinedUserNames.push(member.user.username);
                 })
@@ -84,14 +74,81 @@ client.on('message', msg => {
             joinedUserNames.length && msg.channel.send(`참여한 멤버: ${joinedUserNames.join(', ')}`);
         }
 
+        if(CMD_NAME === 'start') {
+            //NOTE: create channels
+            joinedUserIds.forEach(userId => {
+                const member = msg.guild.members.cache.get(userId);
+                msg.guild.channels.create(`${member.user.username}`, {
+                    permissionOverwrites: joinedUserIds.map(innerUserId => {
+                        if(innerUserId === userId) {
+                            return {
+                                id: innerUserId
+                            }
+                        }
+                        return {
+                            id: innerUserId,
+                            deny: ["VIEW_CHANNEL", "SEND_MESSAGES"]
+                        }
+                    }),
+
+                }).then(channel => {
+                    createdChannelIds.push(channel.id);
+                    channel.send('이 채널은 당신에게만 보이는 채널입니다.');
+                    userId === mafiaId && channel.send('당신은 마피아입니다. 밤이되면 죽이고 싶은 사람에게 송장 역할을 부여하세요.');
+                })
+            })
+
+            //NOTE: random mafia
+            const randomIndex = Math.floor(Math.random() * joinedUserIds.length);
+            mafiaId = joinedUserIds[randomIndex];
+            const mafia = msg.guild.members.cache.get(mafiaId);
+            console.log('mafia name: ', mafia.user.username);
+
+            //NOTE : make roles
+            //FIXME : fix position issue
+            msg.guild.roles.create({
+                data: {
+                    name: '송장',
+                    position: joinedUserIds.length+1,
+                    color: 'DARK_GREY',
+                    permissions: []
+                }
+            }).then(role => {
+                createdRoleIds.push(role.id);
+            })
+            
+            joinedUserIds.forEach(userId => {
+                msg.guild.roles.create({
+                    data: {
+                        name: `role[${userId}]`,
+                        position: userId === mafiaId ? joinedUserIds.length : 1,
+                        color: 'GOLD',
+                        permissions: userId === mafiaId ? ['MANAGE_ROLES'] : []
+                    }
+                }).then(role => {
+                    createdRoleIds.push(role.id);
+                    const member = msg.guild.members.cache.get(userId);
+                    member.roles.add(role.id);
+                });
+            })
+            
+        }
+
         if(CMD_NAME === 'end') {
             //NOTE : initiate vars
-            joinedUsers = [];
+            joinedUserIds = [];
             isCreated = false;
 
-            //TODO : find roles that contain 'role' and delete roles
-            const role = msg.guild.roles.cache.find(role => role.name == 'role[0]');
-            role.delete();
+            //NOTE: delete created channels
+            createdChannelIds.forEach(id => {
+                msg.guild.channels.cache.get(id).delete();
+            })
+            
+            //NOTE: delete created roles
+            createdRoleIds.forEach(id => {
+                msg.guild.roles.cache.get(id).delete();
+            })
+            
         }
     }
   });
